65b209e82e702b688bb4db6b15c99e62be5add19
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2020-02-21 15:18:12 +0000

    copy editor configuration instead of symlinking

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/scrooloose/nerdtree.git $HOME/.vim/pack/dist/start/
 git clone https://github.com/junegunn/fzf.vim $HOME/.vim/pack/dist/start/fzf.vim
 git clone https://github.com/nvie/vim-flake8 $HOME/.vim/pack/dist/start/vim-flake8
 
-# aliases
+# configuration file aliases
 ln -s $PWD/.zsh_aliases $HOME/.zsh_aliases
 ln -s $PWD/.zsh_functions $HOME/.zsh_functions
 ln -s $PWD/.zsh_autocomplete $HOME/.zsh_autocomplete
@@ -13,10 +13,14 @@ ln -s $PWD/.zshrc $HOME/.zshrc
 ln -s $PWD/.gitconfig $HOME/.gitconfig
 ln -s $PWD/.gitignore_global $HOME/.gitignore_global
 ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD/sublime-settings "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-ln -s $PWD/vs-code-settings "$HOME/Library/Application Support/Code/User/settings.json"
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 ln -s $PWD/.screenrc $HOME/.screenrc
+
+# editor configuration
+# copy configuration instead of symlinking. editors like to reorganize it, so
+# it often appears as changed when in fact it hasn't
+cp $PWD/sublime-settings "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+cp $PWD/vs-code-settings "$HOME/Library/Application Support/Code/User/settings.json"
 
 # how to install vscode extensions automatically?
 # github plus theme, gitlens, go, material theme icons, python, terminal
